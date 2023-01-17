b940fdb84dc0005794f7d8f4e99ae2643195b706
DanielCS17 <102037535+DanielCS17@users.noreply.github.com>
2023-01-17 14:01:22 +0000

    Feature: Se ha terminado el experimento dos

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,10 @@ public class Main {
     public static void main(String[] args) {
         Huerto huerto = new Huerto(5);
 
-        Productor productor = new Productor("Don Bosco", 10, 5, huerto);
-        Cliente daniel = new Cliente("Daniel", 5, 5, huerto);
-        Cliente alberto = new Cliente("Alberto", 3, 5, huerto);
+        Productor productor = new Productor("Don Bosco", 5, 4, huerto);
+        Cliente daniel = new Cliente("Daniel", 5, 8, huerto);
 
         productor.start();
         daniel.start();
-        alberto.start();
     }
 }
